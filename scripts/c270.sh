1fbc2baac3c298f212adf84e5326a593d829354b
Soham Patil <sohampatil45939@gmail.com>
2024-11-09 11:01:42 +0530

    added c270 udev rule and readme instructions

---
+++ scripts/c270.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Define the vendor and product IDs for C270 Device
+U2D2_VENDOR_ID="046d"
+U2D2_PRODUCT_ID="0825"
+
+# Define the udev rule content for C270
+RULE_CONTENT="SUBSYSTEM==\"video4linux\", ATTRS{idVendor}==\"$U2D2_VENDOR_ID\", ATTRS{idProduct}==\"$U2D2_PRODUCT_ID\", SYMLINK+=\"docker/c270\""
+
+# Write the rule content to the rule file (overwrite mode)
+echo "$RULE_CONTENT" | sudo tee /etc/udev/rules.d/99-c270.rules > /dev/null
+
+# Reload the udev rules
+sudo udevadm control --reload-rules
+sudo udevadm trigger
+
+echo "Udev rule for Logitech C270 created successfully!"
