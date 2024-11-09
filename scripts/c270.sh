45983853703798fca8636c054f9e53439d0a9352
Soham Patil <sohampatil45939@gmail.com>
2024-11-09 12:54:19 +0530

    updated symlinked name

--- scripts/c270.sh
+++ scripts/c270.sh
@@ -5,7 +5,7 @@ U2D2_VENDOR_ID="046d"
 U2D2_PRODUCT_ID="0825"
 
 # Define the udev rule content for C270
-RULE_CONTENT="SUBSYSTEM==\"video4linux\", ATTRS{idVendor}==\"$U2D2_VENDOR_ID\", ATTRS{idProduct}==\"$U2D2_PRODUCT_ID\", SYMLINK+=\"docker/c270\""
+RULE_CONTENT="SUBSYSTEM==\"video4linux\", ATTRS{idVendor}==\"$U2D2_VENDOR_ID\", ATTRS{idProduct}==\"$U2D2_PRODUCT_ID\", SYMLINK+=\"c270\""
 
 # Write the rule content to the rule file (overwrite mode)
 echo "$RULE_CONTENT" | sudo tee /etc/udev/rules.d/99-c270.rules > /dev/null
