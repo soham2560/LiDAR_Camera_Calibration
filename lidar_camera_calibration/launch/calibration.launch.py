c16bc98f37434d15270e85a5a94591b10259650e
RoCon RRC <rocon.rrc@gmail.com>
2024-12-08 03:15:01 +0530

    made rviz default to true

--- lidar_camera_calibration/launch/calibration.launch.py
+++ lidar_camera_calibration/launch/calibration.launch.py
@@ -30,7 +30,7 @@ def generate_launch_description():
     declared_arguments.append(
         DeclareLaunchArgument(
             'use_rviz',
-            default_value='False',
+            default_value='True',
             description='Launch RVIZ on startup'))
     declared_arguments.append(
         DeclareLaunchArgument(
